219b7c1d76cfb26eb349d45fcb6eaa5a7d8aa4bb
Facto <33882218+Sniped@users.noreply.github.com>
2019-03-10 11:31:58 -0400

    some changes, tester joins the battle!

--- phone.js
+++ phone.js
@@ -9,9 +9,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const dbclient = new Discord.Client();
 const dkclient = new Discord.Client();
+const dtclient = new Discord.Client();
 const vclient = new Voice(config.sid, config.authtoken);
 dbclient.login(config.btoken);
 dkclient.login(config.ktoken);
+dtclient.login(config.ttoken);
 
 dbclient.on('ready', () => {
     console.log(`Brock bot is ready! USERNAME: ${dbclient.user.tag}`);
@@ -21,12 +23,16 @@ dkclient.on('ready', () => {
     console.log(`Kenneth bot is ready! USERNAME: ${dkclient.user.tag}`);
 });
 
+dtclient.on('ready', () => {
+    console.log(`Test bot is ready! USERNAME: ${dtclient.user.tag}`);
+});
+
 dbclient.on('message', msg => {
     if (msg.content.startsWith('ping')) {
         if (msg.author.bot) return;
         msg.channel.send('ping!');
     } else if (msg.channel.id == config.channel) {
-        vclient.messages.create({ body: `${msg.author.tag} BOT: ${msg.author.bot}\n\n${msg.content}`, from: config.bsendernumber, to: config.brocknumber });
+        vclient.messages.create({ body: `${msg.author.tag} ${getBotStatus(msg.author)}\n\n${msg.content}`, from: config.bsendernumber, to: config.brocknumber });
     }
 });
 
@@ -35,9 +41,25 @@ dkclient.on('message', msg => {
         if (msg.author.bot) return;
         msg.channel.send('ping!');
     } else if (msg.channel.id == config.channel) {
-        vclient.messages.create({ body: `${msg.author.tag} BOT: ${msg.author.bot}\n\n${msg.content}`, from: config.ksendernumber, to: config.kennethnumber });
+        vclient.messages.create({ body: `${msg.author.tag} ${getBotStatus(msg.author)}\n\n${msg.content}`, from: config.ksendernumber, to: config.kennethnumber });
     }
 });
+
+dtclient.on('message', msg => {
+    if (msg.content.startsWith('ping')) {
+        if (msg.author.bot) return;
+        msg.channel.send('ping!');
+    } else if (msg.channel.id == config.channel) {
+        vclient.messages.create({ body: `${msg.author.tag} ${getBotStatus(msg.author)}\n\n${msg.content}`, from: config.tsendernumber, to: config.testnumber });
+    }
+});
+
+function getBotStatus(user) {
+    let bot;
+    if (user.bot == true) bot = '[BOT]'
+    if (user.bot == false) bot = ''
+    return bot;
+}
 
 app.get('/', (req, res) => {
     res.send('who tf r u lol');
@@ -49,6 +71,10 @@ app.post('/sms/brock', (req, res) => {
 
 app.post('/sms/kenneth', (req, res) => {
     dkclient.channels.get(config.channel).send(req.body.Body);
+});
+
+app.post('/sms/test', (req, res) => {
+    dtclient.channels.get(config.channel).send(req.body.Body);
 });
 
 app.listen(4768, () => {
